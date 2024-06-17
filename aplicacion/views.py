65f1cc6326705fee79b6660462c5e5f0c63f1cd6
MrDrenix <to.sotomayor@duocuc.cl>
2024-06-17 00:30:19 -0400

    views

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def arbustos(request):
+    return render(request, 'arbustos.html/')
+
+def contacto(request):
+    return render(request, 'contacto.html/')
+
+def flores(request):
+    return render(request, 'flores.html/')
+
+def index(request):
+    return render(request, 'index.html/')
+
+def maceteros(request):
+    return render(request, 'maceteros.html/')
+
+def registro(request):
+    return render(request, 'registro.html/')
+
+def tierrahoja(request):
+    return render(request, 'tierrahoja.html/')

36289ba43ab24d931fb823e11818ac10fdfc459a
gangaxx <jor.oyanedel@duocuc.cl>
2024-06-17 01:19:28 -0400

    commit

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,10 @@
+# aplicacion/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def registro(request):
+    # Add your registration logic here
+    return render(request, 'registro.html')

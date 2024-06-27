7b9b13335f9707a25bd58e9901a2670719043b01
gangaxx <jor.oyanedel@duocuc.cl>
2024-06-27 18:32:05 -0400

    se creo base de datos

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+#tipousuario si va
+class TipoUsuario(models.Model):
+    IdTipoUsuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+#usuario si va
+class Usuario(models.Model):
+    IdUsuario = models.AutoField(primary_key=True)
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+#tipo producto si va
+class TipoProducto(models.Model):
+    IdTipoProducto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+#producto si va
+class Producto(models.Model):
+    IdProducto = models.AutoField(primary_key=True)
+    tipo_producto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    imagen = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+#va en el examen
+class Suscripcion(models.Model):
+    IdSuscripcion = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+
+    def __str__(self):
+        return f"Suscripcion de {self.usuario.nombre}"
+
+#va en el examen
+class Compra(models.Model):
+    IdCompra = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_compra = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Compra {self.IdCompra} de {self.usuario.nombre}"
+
+#va en el examen
+class DetalleCompra(models.Model):
+    IdDetalleCompra = models.AutoField(primary_key=True)
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Detalle de compra {self.IdDetalleCompra} para compra {self.compra.IdCompra}"
+    
+
+

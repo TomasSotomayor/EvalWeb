bbc0c27eaf0314c45e9327b8e2128d6b399241b3
tumami7654321@gmail.com <tumami7654321@gmail.com>
2024-06-29 20:00:08 -0400

    m

--- aplicacion/forms.py
+++ aplicacion/forms.py
@@ -7,3 +7,15 @@ class TipoUsuarioForm(forms.ModelForm):
     class Meta:
         model = TipoUsuario
         fields = ['nombre']
+
+class TipoProductoForm(forms.ModelForm):
+    class Meta:
+        model = TipoProducto
+        fields = ['nombre']
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['nombre','email','password','tipo_usuario']
+
+        
